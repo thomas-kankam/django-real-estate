de6e5c66f78b29e454cdadeb02782c30275c583d
Thomas Kankam <kankamthomas6@gmail.com>
2022-07-14 16:32:35 +0000

    chore configure django logging

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("adminhub/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
