50ce9fae6e06ec84f1b0720c9e263a202f259f88
Thomas Kankam <kankamthomas6@gmail.com>
2022-09-13 16:57:53 +0000

    feat: user auth-register, login, password reset

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -2,6 +2,17 @@
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 from .base import * 
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@real-estate.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
+
+
 DATABASES = {
     "default": {
         "ENGINE": env("POSTGRES_ENGINE"),
