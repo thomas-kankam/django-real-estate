a8e386443cd8d7e9a7d5fb4a6beed230db7af157
Thomas Kankam <kankamthomas6@gmail.com>
2022-09-14 13:07:35 +0000

    feat: add CRUD functionality to properties

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -20,7 +20,7 @@ class PropertySerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "user",
-            "profile_photo",
+            # "profile_photo",
             "title",
             "slug",
             "ref_code",
